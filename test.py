ec96847b864d73710b73fe5ccc76a81423c0d2db
InitNya <nnepipatvink@gmail.com>
2025-02-14 04:36:10 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,21 @@
+from math import ceil
 def transposition_encrypt(plaintext):
 
 
-    lines=len(plaintext)//3
+    lines=ceil(len(plaintext)/3)
 
-    table = [['' for _ in range(3)] for _ in range(lines)]
-    print(table)
+    table = [[' ' for _ in range(3)] for _ in range(lines)]
 
     text_l=list(plaintext)
+
     index=0
     for i in range(lines):
         for j in range(3):
             table[i][j]=text_l[index]
             index+=1
+            if index==len(text_l):
+                break
 
-    print('abaoba:', table)
 
     ciphertext = ''
     for j in range(3):
@@ -25,23 +27,21 @@ def transposition_encrypt(plaintext):
 
 def transposition_decrypt(ciphertext):
     converted=[]
-    lines = len(ciphertext)//3
+    lines = ceil(len(ciphertext)/3)
     text_l=list(ciphertext)
-    table = [['' for _ in range(3)] for _ in range(lines)]
+    table = [[' ' for _ in range(3)] for _ in range(lines)]
     index=0
     for i in range (3):
         for j in range(lines):
             table[j][i]=text_l[index]
             index+=1
-            print('jopa', table)
-    print(table)
     for i in table:
         union=''.join(i)
         converted.append(union)
     decrypted_text = ''.join(converted)
     return decrypted_text
 
-plaintext = "ATTACKATDAWN"
+plaintext = "ATTACKATDAW"
 columns = 3
 
 ciphertext = transposition_encrypt(plaintext)
